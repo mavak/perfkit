7ea4c4fe4277fbceb4093682d02c2d72db1e0f66
Christian Hergert <chris@dronelabs.com>
2010-11-08 00:27:22 -0500

    [profiler] Add documentation to PpgRuler.

--- perfkit-profiler/ppg-ruler.c
+++ perfkit-profiler/ppg-ruler.c
@@ -20,9 +20,9 @@
 
 #include "ppg-ruler.h"
 
-#define ARROW_SIZE 17
-
 G_DEFINE_TYPE(PpgRuler, ppg_ruler, PPG_TYPE_HEADER)
+
+#define ARROW_SIZE 17
 
 struct _PpgRulerPrivate
 {
@@ -46,18 +46,39 @@ enum
 	PROP_POSITION,
 };
 
+/**
+ * ppg_ruler_contains:
+ * @ruler: (in): A #PpgRuler.
+ * @value: (in): A #gdouble containing the time.
+ *
+ * Checks to see if the rulers region contains @value.
+ *
+ * Returns: %TRUE if @value is within @ruler<!-- -->'s region.
+ * Side effects: None.
+ */
 static inline gboolean
 ppg_ruler_contains (PpgRuler *ruler,
                     gdouble   value)
 {
-	PpgRulerPrivate *priv = ruler->priv;
-	return ((value >= priv->lower) && (value <= priv->upper));
+	return ((value >= ruler->priv->lower) &&
+	        (value <= ruler->priv->upper));
 }
 
+/**
+ * ppg_ruler_update_layout_text:
+ * @ruler: (in): A #PpgRuler.
+ * @layout: (in): A #PangoLayout.
+ * @t: (in): A #gdouble containing the time.
+ *
+ * Updates the text property for @layout using the time represented by @t.
+ *
+ * Returns: None.
+ * Side effects: None.
+ */
 static inline void
-ppg_ruler_update_layout_text (PpgRuler *ruler,
+ppg_ruler_update_layout_text (PpgRuler    *ruler,
                               PangoLayout *layout,
-                              gdouble t)
+                              gdouble      t)
 {
 	gchar *markup;
 
@@ -69,6 +90,18 @@ ppg_ruler_update_layout_text (PpgRuler *ruler,
 	g_free(markup);
 }
 
+/**
+ * ppg_ruler_set_range:
+ * @ruler: (in): A #PpgRuler.
+ * @lower: (in): The lower value for the range.
+ * @upper: (in): The upper value for the range.
+ * @position: (in): The current position in the range.
+ *
+ * Sets the visible range for the ruler.
+ *
+ * Returns: None.
+ * Side effects: None.
+ */
 void
 ppg_ruler_set_range (PpgRuler *ruler,
                      gdouble   lower,
@@ -93,6 +126,16 @@ ppg_ruler_set_range (PpgRuler *ruler,
 	g_object_notify(G_OBJECT(ruler), "position");
 }
 
+/**
+ * ppg_ruler_set_position:
+ * @ruler: (in): A #PpgRuler.
+ * @position: (in): The position.
+ *
+ * Sets the position of the arrow in the ruler.
+ *
+ * Returns: None.
+ * Side effects: None.
+ */
 static void
 ppg_ruler_set_position (PpgRuler *ruler,
                         gdouble   position)
@@ -104,6 +147,16 @@ ppg_ruler_set_position (PpgRuler *ruler,
 	g_object_notify(G_OBJECT(ruler), "position");
 }
 
+/**
+ * ppg_ruler_set_lower:
+ * @ruler: (in): A #PpgRuler.
+ * @lower: (in): A #gdouble containing the lower bounds.
+ *
+ * Sets the lower range of the ruler.
+ *
+ * Returns: None.
+ * Side effects: None.
+ */
 static void
 ppg_ruler_set_lower (PpgRuler *ruler,
                      gdouble   lower)
@@ -115,6 +168,16 @@ ppg_ruler_set_lower (PpgRuler *ruler,
 	g_object_notify(G_OBJECT(ruler), "lower");
 }
 
+/**
+ * ppg_ruler_set_upper:
+ * @ruler: (in): A #PpgRuler.
+ * @upper: (in): A #gdouble containing the upper bounds.
+ *
+ * Sets the upper range of the ruler.
+ *
+ * Returns: None.
+ * Side effects: None.
+ */
 static void
 ppg_ruler_set_upper (PpgRuler *ruler,
                      gdouble   upper)
@@ -126,8 +189,18 @@ ppg_ruler_set_upper (PpgRuler *ruler,
 	g_object_notify(G_OBJECT(ruler), "upper");
 }
 
+/**
+ * ppg_ruler_motion_notify_event:
+ * @ruler: (in): A #PpgRuler.
+ *
+ * Handle the "motion-notify-event" for the #PpgRuler. The position of the
+ * arrow is updated.
+ *
+ * Returns: %FALSE always.
+ * Side effects: None.
+ */
 static gboolean
-ppg_ruler_motion_notify_event (GtkWidget *widget,
+ppg_ruler_motion_notify_event (GtkWidget      *widget,
                                GdkEventMotion *motion)
 {
 	PpgRulerPrivate *priv;
@@ -140,12 +213,22 @@ ppg_ruler_motion_notify_event (GtkWidget *widget,
 	priv = ruler->priv;
 
 	gtk_widget_get_allocation(widget, &alloc);
-	pos = priv->lower + (motion->x / alloc.width * (priv->upper - priv->lower));
+	pos = priv->lower +
+	      (motion->x / alloc.width * (priv->upper - priv->lower));
 	ppg_ruler_set_position(ruler, pos);
 
 	return FALSE;
 }
 
+/**
+ * ppg_ruler_draw_arrow:
+ * @ruler: (in): A #PpgRuler.
+ *
+ * Draw the arrow to a pixmap for rendering on top of the background.
+ *
+ * Returns: None.
+ * Side effects: None.
+ */
 static void
 ppg_ruler_draw_arrow (PpgRuler *ruler)
 {
@@ -269,6 +352,17 @@ get_x_offset (PpgRulerPrivate *priv,
 	return floor((b / a) * alloc->width);
 }
 
+/**
+ * ppg_ruler_draw_ruler:
+ * @ruler: (in): A #PpgRuler.
+ *
+ * Draws the background of the ruler containing the time values and ticks
+ * to an offscreen pixmap that can be blitted to the widget during
+ * "expose-event".
+ *
+ * Returns: None.
+ * Side effects: None.
+ */
 static void
 ppg_ruler_draw_ruler (PpgRuler *ruler)
 {
@@ -325,7 +419,7 @@ ppg_ruler_draw_ruler (PpgRuler *ruler)
 		cairo_line_to(cr, x + 0.5, 0.5);
 
 		/*
-		 * TODO: Mini lines.
+		 * Mini lines.
 		 */
 		for (p = v, n = 0, z = 0;
 		     p < v + every;
@@ -355,6 +449,15 @@ ppg_ruler_draw_ruler (PpgRuler *ruler)
 	cairo_destroy(cr);
 }
 
+/**
+ * ppg_ruler_realize:
+ * @widget: (in): A #PpgRuler.
+ *
+ * Handle the "realize" event for the widget.
+ *
+ * Returns: None.
+ * Side effects: None.
+ */
 static void
 ppg_ruler_realize (GtkWidget *widget)
 {
@@ -382,9 +485,18 @@ ppg_ruler_realize (GtkWidget *widget)
 	gdk_drawable_set_colormap(priv->arrow, colormap);
 }
 
-
+/**
+ * ppg_ruler_expose_event:
+ * @ruler: (in): A #PpgRuler.
+ *
+ * Handle the "expose-event" for the widget. Blit the background and position
+ * arrow to the surface.
+ *
+ * Returns: None.
+ * Side effects: None.
+ */
 static gboolean
-ppg_ruler_expose_event (GtkWidget *widget,
+ppg_ruler_expose_event (GtkWidget      *widget,
                         GdkEventExpose *expose)
 {
 	PpgRuler *ruler = (PpgRuler *)widget;
@@ -403,16 +515,31 @@ ppg_ruler_expose_event (GtkWidget *widget,
 	gtk_widget_get_allocation(widget, &alloc);
 	cr = gdk_cairo_create(expose->window);
 
+	/*
+	 * Clip to exposure region.
+	 */
+	gdk_cairo_rectangle(cr, &expose->area);
+	cairo_clip(cr);
+
+	/*
+	 * Render the contents immediately if needed.
+	 */
 	if (priv->dirty) {
 		ppg_ruler_draw_arrow(ruler);
 		ppg_ruler_draw_ruler(ruler);
 		priv->dirty = FALSE;
 	}
 
+	/*
+	 * Blit the background to the surface.
+	 */
 	cairo_rectangle(cr, 0, 0, alloc.width, alloc.height);
 	gdk_cairo_set_source_pixmap(cr, priv->ruler, 0, 0);
 	cairo_fill(cr);
 
+	/*
+	 * Blit the arrow to the surface.
+	 */
 	x = (gint)(((priv->pos - priv->lower) /
                 (priv->upper - priv->lower) *
                 alloc.width) - (ARROW_SIZE / 2.0));
@@ -421,8 +548,10 @@ ppg_ruler_expose_event (GtkWidget *widget,
 	cairo_rectangle(cr, x, y, ARROW_SIZE, ARROW_SIZE);
 	cairo_fill(cr);
 
+	/*
+	 * Cleanup.
+	 */
 	cairo_destroy(cr);
-
 	return FALSE;
 }
 
@@ -457,6 +586,16 @@ ppg_ruler_finalize (GObject *object)
 	G_OBJECT_CLASS(ppg_ruler_parent_class)->finalize(object);
 }
 
+/**
+ * ppg_ruler_size_request:
+ * @ruler: (in): A #PpgRuler.
+ *
+ * Handle the "size-request" for the widget. Make sure we have enough space
+ * to render the time labels as well as the ticks.
+ *
+ * Returns: None.
+ * Side effects: None.
+ */
 static void
 ppg_ruler_size_request (GtkWidget *widget,
                         GtkRequisition *req)
@@ -492,8 +631,18 @@ ppg_ruler_size_request (GtkWidget *widget,
 	}
 }
 
+/**
+ * ppg_ruler_size_allocate:
+ * @ruler: (in): A #PpgRuler.
+ *
+ * Handle the "size-allocate" for the #GtkWidget. The pixmap for the
+ * background is created and drawn if necessary.
+ *
+ * Returns: None.
+ * Side effects: None.
+ */
 static void
-ppg_ruler_size_allocate (GtkWidget *widget,
+ppg_ruler_size_allocate (GtkWidget     *widget,
                          GtkAllocation *alloc)
 {
 	PpgRuler *ruler = (PpgRuler *)widget;
@@ -521,6 +670,15 @@ ppg_ruler_size_allocate (GtkWidget *widget,
 	}
 }
 
+/**
+ * ppg_ruler_style_set:
+ * @ruler: (in): A #PpgRuler.
+ *
+ * Handle the "style-set" event and force a redraw of the widget content.
+ *
+ * Returns: None.
+ * Side effects: None.
+ */
 static void
 ppg_ruler_style_set (GtkWidget *widget,
                      GtkStyle *old_style)
