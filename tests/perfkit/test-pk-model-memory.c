9d1e383d857c469ca2ec786a5d2b32c58badc6dc
Christian Hergert <chris@dronelabs.com>
2010-12-28 19:54:41 -0800

    [tests] Add test for pk_model_insert_manifest()

--- tests/perfkit/test-pk-model-memory.c
+++ tests/perfkit/test-pk-model-memory.c
@@ -1,11 +1,23 @@
 #include <perfkit/perfkit.h>
 
+#include "manifest.h"
+#include "sample.h"
+
 static void
-test_PkModelMemory_new (void)
+test_PkModelMemory_insert_manifest (void)
 {
+	PkManifest *manifest;
 	PkModel *model;
+
+	manifest = pk_manifest_new_from_data(manifest_data, sizeof(manifest_data));
+	g_assert(manifest);
+
 	model = g_object_new(PK_TYPE_MODEL_MEMORY, NULL);
 	g_assert(model);
+
+	pk_model_insert_manifest(model, manifest);
+
+	pk_manifest_unref(manifest);
 	g_object_unref(model);
 }
 
@@ -16,7 +28,7 @@ main (gint   argc,
 	g_setenv("PERFKIT_CONNECTIONS_DIR", PERFKIT_CONNECTIONS_DIR, FALSE);
 	g_type_init();
 	g_test_init(&argc, &argv, NULL);
-	g_test_add_func("/PkModelMemory/new",
-	                test_PkModelMemory_new);
+	g_test_add_func("/PkModelMemory/insert_manifest",
+	                test_PkModelMemory_insert_manifest);
 	return g_test_run();
 }
